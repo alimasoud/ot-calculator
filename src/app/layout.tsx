932458f9457c749b2cf901ae4c7195e4aa2da3b8
Ali Masaud <dev.ali.masoud@gmail.com>
2025-05-02 14:09:34 +0300

    add image for meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,31 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bahrain Overtime Calculator",
-  description: "Bahrain Overtime Calculator app to calculate your overtime pay easily.",
+  title: "Bahrain Over Time Calculator",
+  description: "Calculate your overtime pay based on Bahrain Labour Law",
+  openGraph: {
+    title: "Bahrain Over Time Calculator",
+    description: "Calculate your overtime pay based on Bahrain Labour Law",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Bahrain Over Time Calculator",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bahrain Over Time Calculator",
+    description: "Calculate your overtime pay based on Bahrain Labour Law",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
@@ -25,9 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
